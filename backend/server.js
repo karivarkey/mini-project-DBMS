f35435e9db86a5443c95836ff46da6fd2902810c
karivarkey <geevargheseregi78@gmail.com>
2024-10-20 00:06:53 +0530

    chore(backend):Initialized express app

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+// api/index.js
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express on Vercel!");
+});
+
+module.exports = app;
