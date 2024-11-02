e56845065b0c92eb8e1765a5140d7b45fa0a856d
karivarkey <geevargheseregi78@gmail.com>
2024-11-02 21:29:36 +0530

    feat(backend):Added add manufacturer

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const ManufacturerSchema = require("./models/manufacturer");
+
 
 const app = express();
+app.use(cors());
+app.use(express.json()); 
+
+
+
 const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI;
 
@@ -12,6 +20,21 @@ mongoose
   .catch((error) =>
     console.error("Error connecting to MongoDB:", error.message)
   );
+
+
+  app.post("/api/manufacturer",async  (request, response) => {
+    
+    
+    const manufacturer = new ManufacturerSchema(request.body);
+    manufacturer.save().then((result) => {
+      response.status(201).send(result);
+    });
+  });
+
+
+
+
+
 
 // Start the server
 app.listen(PORT, () => {
