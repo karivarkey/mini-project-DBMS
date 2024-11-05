e2f7daa9e5a58fabc383e03c68b91050b5bcbb81
karivarkey <geevargheseregi78@gmail.com>
2024-11-05 20:35:07 +0530

    feat(frontend):Landing page added search bar

---
+++ frontend/src/pages/landing/components/search/search.tsx
@@ -0,0 +1,21 @@
+const Search = () => {
+  return (
+    <div className="w-full flex justify-center items-center gap-10">
+      <div className="w-1/2">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full px-4 py-1 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-500 rounded-full text-left items-center"
+        />
+      </div>
+      <button className="px-4  bg-secondary-gray  rounded-full hover:bg-[#a1a1a1] flex gap-3 items-center text-black font-poppins font-light transition-all duration-300 ease-in-out">
+        <p>Filters</p>
+        <div className="bg-white w-[1.5px] h-10"></div>
+
+        <img src="/landing/filter.svg" alt="asdas" />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Search from "./components/search/search";
 
 const Landing = () => {
   const [data, setData] = useState<any>(null); // Using `any` for the data state
@@ -19,21 +20,27 @@ const Landing = () => {
   }, []);
 
   return (
-    <div className="relative">
-      <img src={data?.imageUrl} alt="New Arrival" className="w-full h-auto" />
-      <div className="absolute inset-0 flex items-center flex-col gap-5 justify-center text-black text-center">
-        <div className="bg-white bg-opacity-70 p-4">
-          <h1 className="text-2xl md:text-5xl font-light font-poppins pb-2">
-            Explore our new Arrival
-          </h1>
-          <h2 className="text-lg md:text-3xl font-bold">
-            {data?.mostPopularToy?.productName} !! {/* Use optional chaining */}
-          </h2>
+    <div className="">
+      <div className="w-full py-2">
+        <Search />
+      </div>
+      <div className="relative">
+        <img src={data?.imageUrl} alt="New Arrival" className="w-full h-auto" />
+        <div className="absolute inset-0 flex items-center flex-col gap-5 justify-center text-black text-center">
+          <div className="bg-white bg-opacity-70 p-4">
+            <h1 className="text-2xl md:text-5xl font-light font-poppins pb-2">
+              Explore our new Arrival
+            </h1>
+            <h2 className="text-lg md:text-3xl font-bold">
+              {data?.mostPopularToy?.productName} !!{" "}
+              {/* Use optional chaining */}
+            </h2>
+          </div>
+          <button className="relative bg-[#FCE1E4] px-10 py-3 rounded-full font-poppins font-regular group hover:bg-white ">
+            <span className="relative z-10">EXPLORE NOW!</span>
+            <span className="absolute left-0 right-0 top-3/4 h-[2px] mx-10 bg-black transition-transform duration-300 transform scale-x-0 group-hover:scale-x-100"></span>
+          </button>
         </div>
-        <button className="relative bg-[#FCE1E4] px-10 py-3 rounded-full font-poppins font-regular group hover:bg-white ">
-          <span className="relative z-10">EXPLORE NOW!</span>
-          <span className="absolute left-0 right-0 top-3/4 h-[2px] mx-10 bg-black transition-transform duration-300 transform scale-x-0 group-hover:scale-x-100"></span>
-        </button>
       </div>
     </div>
   );
