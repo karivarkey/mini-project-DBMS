393dd9e2c89277f94c1c60af9acaca499f8e3696
karivarkey <geevargheseregi78@gmail.com>
2024-11-05 20:59:16 +0530

    feat(frontend):Updated to fit mobile devices

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -25,19 +25,24 @@ const Landing = () => {
         <Search />
       </div>
       <div className="relative">
-        <img src={data?.imageUrl} alt="New Arrival" className="w-full h-auto" />
-        <div className="absolute inset-0 flex items-center flex-col gap-5 justify-center text-black text-center">
-          <div className="bg-white bg-opacity-70 p-4">
-            <h1 className="text-2xl md:text-3xl font-light font-poppins pb-2">
+        <img
+          src={data?.imageUrl}
+          alt="New Arrival"
+          className="w-full  min-h-56 object-cover"
+        />
+        <div className="absolute inset-0 flex items-center flex-col gap-4 justify-center text-black text-center p-4 md:p-8">
+          <div className="bg-white bg-opacity-70 p-4 rounded-lg">
+            <h1 className="text-xl md:text-3xl font-light font-poppins pb-1">
               Explore our new Arrival
             </h1>
-            <h2 className="text-lg md:text-3xl font-semibold">
-              {data?.mostPopularToy?.productName} !!{" "}
-              {/* Use optional chaining */}
+            <h2 className="text-md md:text-2xl font-semibold">
+              {data?.mostPopularToy?.productName} !!
             </h2>
           </div>
-          <button className="relative bg-[#FCE1E4] px-10 py-3 rounded-full font-poppins font-regular group hover:bg-white ">
-            <span className="relative z-10">EXPLORE NOW!</span>
+          <button className="relative bg-[#FCE1E4] px-6 py-2 rounded-full font-poppins font-regular group hover:bg-white transition duration-300">
+            <span className="relative z-10 text-sm md:text-base">
+              EXPLORE NOW!
+            </span>
             <span className="absolute left-0 right-0 top-3/4 h-[2px] mx-10 bg-black transition-transform duration-300 transform scale-x-0 group-hover:scale-x-100"></span>
           </button>
         </div>
