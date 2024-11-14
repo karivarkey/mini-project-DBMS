c763a0c5f1aa5fe64c47d6dadad61f701688d972
karivarkey <geevargheseregi78@gmail.com>
2024-11-14 20:47:35 +0530

    chore(build error)

--- frontend/src/pages/sell/index.tsx
+++ frontend/src/pages/sell/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import OrderCard from "./components/orderCard/orderCard";
 import { Order } from "../../types/Order";
 
